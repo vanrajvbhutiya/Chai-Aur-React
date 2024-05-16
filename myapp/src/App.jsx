ce878c76f374e8da0367953518f63abb206c8350
vanraj bhutiya <vanrajvbhutiya@gmail.com>
2024-05-16 13:56:03 +0530

    First Project Is Started

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -4,13 +4,10 @@ function App() {
   const [count, setCount] = useState(0)
 
 
+
   return (
     <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-       </div>
+        <h1>Hello World</h1>
     </>
   )
 }
